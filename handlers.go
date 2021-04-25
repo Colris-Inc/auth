6826d8722d5a881425e7f0ac325fa3ceaaca1651
Jerry Devin <jerrydevin96@gmail.com>
2021-04-25 13:44:57 +0530

    api endpoints created

--- handlers.go
+++ handlers.go
@@ -5,3 +5,39 @@ func registerUserHandler(jsonString string) string {
 
 	return response
 }
+
+func loginHandler(jsonString string) string {
+	var response string
+
+	return response
+}
+
+func logoutHandler(jsonString string) string {
+	var response string
+
+	return response
+}
+
+func userInfoHandler(jsonString string) string {
+	var response string
+
+	return response
+}
+
+func refreshHandler(jsonString string) string {
+	var response string
+
+	return response
+}
+
+func updatePasswordHandler(jsonString string) string {
+	var response string
+
+	return response
+}
+
+func updateUserHandler(jsonString string) string {
+	var response string
+
+	return response
+}
--- main.go
+++ main.go
@@ -14,12 +14,12 @@ func main() {
 
 	http.HandleFunc("/v0/registerUser", registerUser)
 	//using register user as dummy for now
-	http.HandleFunc("/v0/login", registerUser)
-	http.HandleFunc("/v0/loout", registerUser)
-	http.HandleFunc("/v0/userInfo", registerUser)
-	http.HandleFunc("/v0/refresh", registerUser)
-	http.HandleFunc("/v0/updatePassword", registerUser)
-	http.HandleFunc("/v0/updateUser", registerUser)
+	http.HandleFunc("/v0/login", login)
+	http.HandleFunc("/v0/loout", logout)
+	http.HandleFunc("/v0/userInfo", userInfo)
+	http.HandleFunc("/v0/refresh", refresh)
+	http.HandleFunc("/v0/updatePassword", updatePassword)
+	http.HandleFunc("/v0/updateUser", updateUser)
 }
 
 func registerUser(w http.ResponseWriter, r *http.Request) {
@@ -35,6 +35,108 @@ func registerUser(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, `{response: POST method expected}`)
 	} else {
 		response := registerUserHandler(string(JSONVal))
+		fmt.Fprint(w, response)
+	}
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	(w).Header().Set("Access-Control-Allow-Origin", "*")
+	JSONVal, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println("[ERROR] " + err.Error())
+		fmt.Fprint(w, `{response: `+err.Error()+`}`)
+	}
+	if r.Method != "POST" {
+		w.WriteHeader(403)
+		fmt.Fprint(w, `{response: POST method expected}`)
+	} else {
+		response := loginHandler(string(JSONVal))
+		fmt.Fprint(w, response)
+	}
+}
+
+func logout(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	(w).Header().Set("Access-Control-Allow-Origin", "*")
+	JSONVal, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println("[ERROR] " + err.Error())
+		fmt.Fprint(w, `{response: `+err.Error()+`}`)
+	}
+	if r.Method != "POST" {
+		w.WriteHeader(403)
+		fmt.Fprint(w, `{response: POST method expected}`)
+	} else {
+		response := logoutHandler(string(JSONVal))
+		fmt.Fprint(w, response)
+	}
+}
+
+func userInfo(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	(w).Header().Set("Access-Control-Allow-Origin", "*")
+	JSONVal, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println("[ERROR] " + err.Error())
+		fmt.Fprint(w, `{response: `+err.Error()+`}`)
+	}
+	if r.Method != "GET" {
+		w.WriteHeader(403)
+		fmt.Fprint(w, `{response: GET method expected}`)
+	} else {
+		response := userInfoHandler(string(JSONVal))
+		fmt.Fprint(w, response)
+	}
+}
+
+func refresh(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	(w).Header().Set("Access-Control-Allow-Origin", "*")
+	JSONVal, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println("[ERROR] " + err.Error())
+		fmt.Fprint(w, `{response: `+err.Error()+`}`)
+	}
+	if r.Method != "POST" {
+		w.WriteHeader(403)
+		fmt.Fprint(w, `{response: POST method expected}`)
+	} else {
+		response := refreshHandler(string(JSONVal))
+		fmt.Fprint(w, response)
+	}
+}
+
+func updatePassword(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	(w).Header().Set("Access-Control-Allow-Origin", "*")
+	JSONVal, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println("[ERROR] " + err.Error())
+		fmt.Fprint(w, `{response: `+err.Error()+`}`)
+	}
+	if r.Method != "POST" {
+		w.WriteHeader(403)
+		fmt.Fprint(w, `{response: POST method expected}`)
+	} else {
+		response := updatePasswordHandler(string(JSONVal))
+		fmt.Fprint(w, response)
+	}
+}
+
+func updateUser(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	(w).Header().Set("Access-Control-Allow-Origin", "*")
+	JSONVal, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println("[ERROR] " + err.Error())
+		fmt.Fprint(w, `{response: `+err.Error()+`}`)
+	}
+	if r.Method != "POST" {
+		w.WriteHeader(403)
+		fmt.Fprint(w, `{response: POST method expected}`)
+	} else {
+		response := updateUserHandler(string(JSONVal))
 		fmt.Fprint(w, response)
 	}
 }
